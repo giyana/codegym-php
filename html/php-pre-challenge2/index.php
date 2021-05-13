740598f709250b0ff0dd9bf9485ad8d2ff227c47
giyana <wktkktkr5@gmail.com>
2021-05-13 23:03:32 +0900

    文法ミス等修正

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -1,14 +1,15 @@
 <?php
-$array = explode(',', $_GET['array']);
+$array = array(3, 2, 1, 4, 15, 18, 13, 99, 77, 66, 1, 100, 0);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-    //バブルソート
-    for ($j = $i - 1; $j < count($array); $j++) {
-        if ($array[$j - 1] > $array[$j]); {
+    //要素個数-1回入れ替えを行う
+    for ($j = 1; $j < count($array); $j++) {
+        //隣と数字入れ替え
+        if ($array[$j - 1] > $array[$j]) {
             $subs = $array[$j];
-            $array[$n] = $array[$n - 1];
-            $array[$n - 1] = $subs;
+            $array[$j] = $array[$j - 1];
+            $array[$j - 1] = $subs;
         }
     }
 }
