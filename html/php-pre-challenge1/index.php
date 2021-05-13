49f68e290e914735fe21e95122b2730aebcd2836
giyana <wktkktkr5@gmail.com>
2021-05-13 22:24:35 +0900

    仮コード作成

--- html/php-pre-challenge1/index.php
+++ html/php-pre-challenge1/index.php
@@ -1,5 +1,19 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
     //ここからコードを書く
-    echo $i;
+    if ($i % 3 == 0) {
+        if ($i % 5 == 0) {
+            print("3の倍数であり、5の倍数");
+            print("<br>");
+        } else {
+            print("3の倍数");
+            print("<br>");
+        }
+    } elseif ($i % 5 == 0) {
+        print("5の倍数");
+        print("<br>");
+    } else {
+        print($i);
+        print("<br>");
+    }
 }
