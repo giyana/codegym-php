3371e67c9aa5edf6cc6dbb9fc07aee4854f451af
giyana <wktkktkr5@gmail.com>
2021-05-13 22:35:56 +0900

    仮コーディング

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,7 +3,14 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+    //バブルソート
+    for ($j = $i - 1; $j < count($array); $j++) {
+        if ($array[$j - 1] > $array[$j]); {
+            $subs = $array[$j];
+            $array[$n] = $array[$n - 1];
+            $array[$n - 1] = $subs;
+        }
+    }
 }
 // 修正はここまで
 
